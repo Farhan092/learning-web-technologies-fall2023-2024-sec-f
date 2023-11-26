83886ac7f50d30506cba21b6d6ec43b04502750f
Farhan Amin <133957646+Farhan092@users.noreply.github.com>
2023-11-26 14:07:54 +0600

    script1.js

--- ajax/script.js
+++ ajax/script.js
@@ -3,7 +3,7 @@ function search() {
   let xhttp = new XMLHttpRequest();
 
   xhttp.open('POST', 'search.php', true);
-  xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
 
   xhttp.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
